ac62bf96d0c4b99ebed4ba2129eb0cdb38001e48
bage2014 <893542907@qq.com>
2020-06-06 23:57:47 +0800

    insert query

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,62 @@
 <template>
+<body>
+
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    刚刚回归吧
+    
+
   </div>
+
+  <div id="wrapper" style="height: 310px;">
+    </div>
+</body>
+
+
 </template>
 
 <script>
+
+  import Chimee from 'chimee'
+// import ChimeeKernelHls from 'chimee-kernel-hls'
+// const chimee = new Chimee({
+//               wrapper: '#wrapper',
+//               src: 'http://39.135.32.24:6610/000000001000/1000000001000023734/index.m3u8',
+//               controls: true,
+//               autoplay: true,
+//               kernels: {
+//                 hls: ChimeeKernelHls,
+//               }
+//             });
+import hls from 'chimee-kernel-hls';
+const chimee = new Chimee({
+  wrapper: '#wrapper',
+  src: 'http://cdn.toxicjohann.com/lostStar.mp4',
+  controls: true,
+  autoplay: true,
+  kernels: {
+    hls
+  }
+});
+
 export default {
   name: 'HelloWorld',
+
+
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+
+    methods: {
+      init() {
+        
+        chimee.play();
+    },
+    },
+
+    created (){
+      this.init();
+    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
